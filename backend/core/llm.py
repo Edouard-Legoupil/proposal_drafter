8d61f6aa6a00e9593f2865b87fea6f1241064c33
Edouard Legoupil <edouard.legoupil@gmail.com>
2025-08-14 00:20:09 +0200

    add gemini

--- backend/core/llm.py
+++ backend/core/llm.py
@@ -1,14 +1,14 @@
 #  Standard Library
 import os
 
-#  Third-Party Libraries
-from langchain_openai import AzureChatOpenAI
-from dotenv import load_dotenv
-
 # Load environment variables from a .env file.
+from dotenv import load_dotenv
 load_dotenv()
 
+
 # --- Azure OpenAI Configuration ---
+#  Third-Party Libraries
+from langchain_openai import AzureChatOpenAI
 
 # Set environment variables required by the Azure OpenAI client.
 os.environ["AZURE_API_TYPE"] = "azure"
@@ -56,3 +56,46 @@ def get_embedder_config():
             "api_version": os.getenv("AZURE_OPENAI_API_VERSION_EMBED", "2023-05-15")
         }
     }
+
+
+# # --- Google Gemini Configuration ---
+
+# #  Third-Party Libraries
+# from crewai import LLM
+
+# # Set environment variables required by Gemini client.
+# os.environ["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
+
+# # Validate that all required environment variables are set.
+# required_vars = [
+#     "GEMINI_API_KEY"
+# ]
+# missing_vars = [var for var in required_vars if not os.getenv(var)]
+# if missing_vars:
+#     raise ValueError(f"Missing required environment variables for LLM: {', '.join(missing_vars)}")
+
+# # --- Language Model Initialization ---
+
+# # Initialize Geminia language model.
+# # This object will be used by the CrewAI agents to interact with the Azure OpenAI service.
+# llm = LLM(
+#      model="gemini/gemini-1.5-flash", # Or "gemini/#gemini-1.5-pro"
+#      temperature=0.4,
+#      api_key=os.getenv("GEMINI_API_KEY") # 
+# )
+
+# def get_embedder_config():
+#     """
+#     Returns the configuration for the Azure OpenAI embedder.
+#     """
+#     return {
+#         "provider": "google",  
+#         "config": {
+#             # Use the Gemini #embedding model
+#             "model": "models/embedding-001", 
+#             # Optional: #Specify task type for optimized embeddings
+#             "task_type": "retrieval_document", 
+#             "api_key": os.getenv("GEMINI_API_KEY") 
+#             # Ensure API #key is passed for embedding as well
+#         }
+#     }
